6881f4b3c3bf550bd5fd646c4699ab2b477460b7
Joel <joelmaxw311@gmail.com>
2019-05-27 05:14:03 -0700

    DJI SDK key is now configurable through a Unity GUI.

--- Assets/DroneAPI/Editor/BuildWindow.cs
+++ Assets/DroneAPI/Editor/BuildWindow.cs
@@ -11,16 +11,24 @@ namespace droneapi
 {
     public class BuildWindow : EditorWindow
     {
-        const string OPERATION_TITLE = "Build with DJI integration";
-        //[SerializeField] private string androidProjectPath = null;
-        [SerializeField] private string androidSdkPath = null;
-        [SerializeField] private string buildJdkPath = null;
-        [SerializeField] private string djiSdkKey = null;
-        [SerializeField] private string[] exportScenes = { "Assets/start.unity", "Assets/setupScene.unity", "Assets/main.unity" };
-
-        private const BuildOptions kDefaultExportOptions = BuildOptions.AllowDebugging | BuildOptions.Development;
-
-        BuildUtil.BuildVariables BuildVars { get { return new BuildUtil.BuildVariables(androidSdkPath, buildJdkPath, djiSdkKey); } }
+        const string OPERATION_TITLE = "Configure DJI SDK";
+        [SerializeField] private string djiSdkKey;
+        
+        public class SdkParams { public string djiSdkKey; };
+        
+        /// <summary>
+        /// External access to certain saved properties of BuildWindow.
+        /// </summary>
+        public static SdkParams SavedParams
+        {
+            get
+            {
+                SdkParams cfg = new SdkParams();
+                var data = EditorPrefs.GetString(OPERATION_TITLE, JsonUtility.ToJson(cfg, true)); // get saved fields of BuildWindow
+                JsonUtility.FromJsonOverwrite(data, cfg); // apply relevant fields to cfg
+                return cfg;
+            }
+        }
 
         [MenuItem("Drone/" + OPERATION_TITLE)]
         static void Init()
@@ -47,110 +55,38 @@ namespace droneapi
         void OnGUI()
         {
             GUILayout.Space(10);
-            EditorGUILayout.LabelField("This dialog is for exporting and building your project with DJI drone control integration.", EditorStyles.wordWrappedLabel);
+            EditorGUILayout.LabelField("Configuration for DJI drone control integration.", EditorStyles.boldLabel);
             GUILayout.Space(20);
 
-            #region Embedded Project Path Field (no longer required - this is now included in the Unity package)
-            /*EditorGUILayout.LabelField("Next, find the Embedded Unity Drone Control project: ", EditorStyles.wordWrappedLabel);
-            string apPath = GUILayout.TextField(this.androidProjectPath);
-            EditorGUILayout.BeginHorizontal();
-            if (GUILayout.Button("Browse file system")) apPath = EditorUtility.OpenFolderPanel("Embedded Unity Drone Control project root", apPath, "");
-            if (GUILayout.Button("Download from GitHub")) Application.OpenURL("https://github.com/SamKrogh/Embedded-Unity-Drone-Control-Tango-Version-");
-            EditorGUILayout.EndHorizontal();
-            this.androidProjectPath = apPath;
-            GUILayout.Space(20);*/
-            #endregion
-
-            #region Android SDK Path Field
-            EditorGUILayout.LabelField("Android SDK Tools: ", EditorStyles.wordWrappedLabel);
-            string localPropFile = Cleanup.kNativeAppDir + "/local.properties";
-            string savedSdkString = androidSdkPath ?? BuildUtil.GetSavedAndroidSdkPath(localPropFile, BuildVars);
-            string sdkPath = GUILayout.TextField(this.androidSdkPath);
-            EditorGUILayout.BeginHorizontal();
-            if (GUILayout.Button("Browse file system")) sdkPath = EditorUtility.OpenFolderPanel("Android SDK tools", sdkPath, "");
-            if (GUILayout.Button("Download")) Application.OpenURL("https://developer.android.com/studio#command-tools");
-            EditorGUILayout.EndHorizontal();
-            if (this.androidSdkPath != sdkPath)
-            {
-                this.androidSdkPath = sdkPath;
-                BuildUtil.WriteAndroidSdkPath(Cleanup.kNativeAppDir, BuildVars);
-            }
-            #endregion
+            djiSdkKey = EditorGUILayout.TextField("SDK Key: ", djiSdkKey);
             GUILayout.Space(20);
 
-            #region JDK Path Field
-            EditorGUILayout.LabelField("JDK Path: ", EditorStyles.wordWrappedLabel);
-            string jdkPath = GUILayout.TextField(this.buildJdkPath);
-            EditorGUILayout.BeginHorizontal();
-            if (GUILayout.Button("Browse file system")) jdkPath = EditorUtility.OpenFolderPanel("JDK Home", jdkPath, "");
-            if (GUILayout.Button("Download")) Application.OpenURL("https://www.oracle.com/technetwork/java/javase/downloads/index.html");
-            EditorGUILayout.EndHorizontal();
-            this.buildJdkPath = jdkPath;
-            #endregion
+            EditorGUILayout.LabelField("DJI SDK Registration", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Every application needs a unique App Key to initialize the SDK.", EditorStyles.wordWrappedLabel);
+            EditorGUILayout.LabelField("To create an App Key for an application:", EditorStyles.wordWrappedLabel);
+            EditorGUILayout.LabelField("Go to the DJI developer Developer Center", EditorStyles.wordWrappedLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.LabelField("• Select the \"Apps\" tab on the left.", EditorStyles.wordWrappedLabel);
+            EditorGUILayout.LabelField("• Select the \"Create App\" button on the right.", EditorStyles.wordWrappedLabel);
+            EditorGUILayout.LabelField("• Enter the name, platform, package identifier, category and description of the application.", EditorStyles.wordWrappedLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.LabelField("• For Android, the package identifier is the Package Name.", EditorStyles.wordWrappedLabel);
+            EditorGUI.indentLevel--;
+            EditorGUILayout.LabelField("• An application activation email will be sent to complete App Key generation.", EditorStyles.wordWrappedLabel);
+            EditorGUILayout.LabelField("• The App Key will appear in the developer center, and can be copied and pasted into the \"SDK Key\" field, above.", EditorStyles.wordWrappedLabel);
+            EditorGUI.indentLevel--;
             GUILayout.Space(20);
 
-            // 1) Export Game
-            if (GUILayout.Button("Export Game Module")) ExportGame(Cleanup.kExportDir, exportScenes);
-            // 2) Build Game
-            if (GUILayout.Button("Build Game Module")) BuildGame();
-            // 3) Build Application
-            if (GUILayout.Button("Build App")) BuildApp();
-            // 4) Install Application
-            if (GUILayout.Button("Install App")) InstallApp();
-        }
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Package Name: ",EditorStyles.boldLabel, GUILayout.MaxWidth(110));
+            EditorGUILayout.LabelField(PlayerSettings.GetApplicationIdentifier(BuildTargetGroup.Android),EditorStyles.textField);
+            EditorGUILayout.EndHorizontal();
 
-        public static void ExportGame(string outputPath, string[] scenes, BuildOptions options = kDefaultExportOptions)
-        {
-            if (!Directory.Exists(outputPath))
-                Directory.CreateDirectory(outputPath);
-
-            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
-            {
-                scenes = scenes,
-                locationPathName = outputPath,
-                target = BuildTarget.Android,
-                options = BuildOptions.AcceptExternalModificationsToPlayer | options
-            };
-
-            var results = BuildPipeline.BuildPlayer(buildPlayerOptions);
-            Debug.Log(System.DateTime.Now.ToShortTimeString() + "   Export completed. See results below: \n" + results);
-        }
-
-        void BuildGame()
-        {
-            var buildVars = BuildVars;
-            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "uwb.xr.unityembed"); // change package name to unityembed
-            BuildUtil.InstallGradlew(Cleanup.kNativeAppDir, Cleanup.ExportedGameDir);
-            string error = BuildUtil.Gradlew(Cleanup.ExportedGameDir, "assembleDebug", buildVars);
-            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, buildVars.PackageName); // change package name back to custom setting
-            bool success = !error.Contains("FAILURE:");
-            string message = System.DateTime.Now.ToShortTimeString() + "   Build game done. See results below: \n" + error;
-            if (success)
-                Debug.Log(message);
-            else
-                Debug.LogError(message);
-        }
-
-        void BuildApp()
-        {
-            string error = BuildUtil.Gradlew(Cleanup.kNativeAppDir, "assembleDebug", BuildVars);
-            bool success = !error.Contains("FAILURE:");
-            string message = System.DateTime.Now.ToShortTimeString() + "   Build app done. See results below: \n" + error;
-            if (success)
-                Debug.Log(message);
-            else
-                Debug.LogError(message);
-        }
-
-        void InstallApp()
-        {
-            string error = BuildUtil.Gradlew(Cleanup.kNativeAppDir, "installDebug", BuildVars);
-            bool success = !error.Contains("FAILURE:");
-            string message = System.DateTime.Now.ToShortTimeString() + "   Install app done. See results below: \n" + error;
-            if (success)
-                Debug.Log(message);
-            else
-                Debug.LogError(message);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Change Package Name in Player Settings")) Selection.activeObject = Unsupported.GetSerializedAssetInterfaceSingleton("PlayerSettings");
+            if (GUILayout.Button("Manage SDK Keys")) Application.OpenURL("http://developer.dji.com/en/user/apps");
+            EditorGUILayout.EndHorizontal();
+            GUILayout.Space(20);
         }
     }
 }
